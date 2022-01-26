3445993f146a44f4500711b5b01c427fa874aa91
Alex <azakotyansky@gmail.com>
2022-01-26 13:36:55 -0500

    Initial commit

---
+++ src/main/java/alex/trading/oms/AlexTradingOmsApplication.java
@@ -0,0 +1,13 @@
+package alex.trading.oms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlexTradingOmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlexTradingOmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/alex/trading/oms/AlexTradingOmsApplicationTests.java
@@ -0,0 +1,13 @@
+package alex.trading.oms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlexTradingOmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
