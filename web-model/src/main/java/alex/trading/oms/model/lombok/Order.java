afa9d050e0bb11b934c45b7ac3be9243556586b4
Alex <azakotyansky@gmail.com>
2022-01-27 18:46:12 -0500

    Initial commit

--- web-model/src/main/java/alex/trading/oms/model/lombok/Order.java
+++ web-model/src/main/java/alex/trading/oms/model/lombok/Order.java
@@ -1,18 +1,23 @@
 package alex.trading.oms.model.lombok;
 
 
-//@Data
-//@Builder
+import lombok.Builder;
+import lombok.Data;
+
+
 
 //@lombok.Getter
 //@lombok.Setter
 
 // to use RequiredArgsConstructor.
 // According to Documentation, Required arguments are final fields and fields with constraints such as @NonNull.
+@Data
+@Builder
+@lombok.NoArgsConstructor
+@lombok.AllArgsConstructor
 //@lombok.RequiredArgsConstructor
-//@lombok.EqualsAndHashCode(of = {"ordId", "ticker", "isBuy"})
+@lombok.EqualsAndHashCode(of = {"ordId", "ticker", "isBuy"})
 public class Order {
-
     private long ordId;
     private long clOrdId;
     private String ticker;
@@ -22,4 +27,14 @@ public class Order {
     private double price;
     private int ordType;
 
+    public static void main(String[] args) {
+        Order order = Order.builder()
+                .ordId(1L)
+                .clOrdId(45L)
+                .ticker("IBM")
+                .isBuy(true)
+                .build();
+
+        System.out.println(order);
+    }
 }
---
+++ web-model/src/test/java/alex/trading/oms/model/lombok/OrderTest.java
@@ -0,0 +1,14 @@
+package alex.trading.oms.model.lombok;
+
+//import static org.junit.jupiter.api.Assertions.*;
+
+class OrderTest {
+
+/*    @org.junit.jupiter.api.Test
+    void canEqual() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void testHashCode() {
+    }*/
+}
