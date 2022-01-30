2791e38362212e446bf778829a9c89db6e95e46b
Alex <azakotyansky@gmail.com>
2022-01-29 20:44:55 -0500

    Initial commit

--- web-model/src/main/java/alex/trading/oms/model/mapper/IJsonMapper.java
+++ web-model/src/main/java/alex/trading/oms/model/mapper/IJsonMapper.java
@@ -1,4 +1,8 @@
 package alex.trading.oms.model.mapper;
 
+import org.mapstruct.factory.Mappers;
+
 public interface IJsonMapper {
+
+    IJsonMapper INSTANCE = Mappers.getMapper(IJsonMapper.class);
 }
--- web-model/src/main/java/alex/trading/oms/model/mapper/IXmlMapper.java
+++ web-model/src/main/java/alex/trading/oms/model/mapper/IXmlMapper.java
@@ -3,11 +3,12 @@ package alex.trading.oms.model.mapper;
 import alex.trading.oms.model.UserRequestMsg;
 import alex.trading.oms.model.lombok.User;
 import alex.trading.oms.model.xml.UserCommand;
+import org.mapstruct.factory.Mappers;
 
 //@Mapper
 public interface IXmlMapper {
 
-    //UserXmlMapper INSTANCE = Mappers.getMapper(UserXmlMapper.class);
+    IXmlMapper INSTANCE = Mappers.getMapper(IXmlMapper.class);
 
     UserRequestMsg userToUserRequestMsg(User user);
     User userRequestMsgToUser(UserRequestMsg userRequestMsg);
