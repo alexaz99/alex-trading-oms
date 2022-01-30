156e83eed2d3ef0d74520f868bafaad9ca0f02ea
Alex <azakotyansky@gmail.com>
2022-01-29 20:48:11 -0500

    Initial commit

--- web-model/src/main/java/alex/trading/oms/model/mapper/IJsonMapper.java
+++ web-model/src/main/java/alex/trading/oms/model/mapper/IJsonMapper.java
@@ -2,6 +2,15 @@ package alex.trading.oms.model.mapper;
 
 import org.mapstruct.factory.Mappers;
 
+/**
+ * Generated mapstruc converter.
+ * Based on an annotation object mapper.
+ * Works by creating an interface for a "mapper", annotation processing
+ * is used to create the implementation.
+ *
+ * Conversion between two POJOs
+ * Create Mappers to convert both ways.
+ */
 public interface IJsonMapper {
 
     IJsonMapper INSTANCE = Mappers.getMapper(IJsonMapper.class);
