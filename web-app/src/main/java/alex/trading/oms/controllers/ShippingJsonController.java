cd133574f82377b2664c3e756326e5947164b941
Alex <azakotyansky@gmail.com>
2022-01-29 20:15:46 -0500

    Initial commit

--- web-app/src/main/java/alex/trading/oms/controllers/ShippingJsonController.java
+++ web-app/src/main/java/alex/trading/oms/controllers/ShippingJsonController.java
@@ -1,10 +1,11 @@
 package alex.trading.oms.controllers;
 
+import alex.trading.oms.model.json.ShippingAddress;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ShippingJsonController {
 
     //public
-    //ShippingAddr
+    ShippingAddress shippingAddress = new ShippingAddress();
 }
