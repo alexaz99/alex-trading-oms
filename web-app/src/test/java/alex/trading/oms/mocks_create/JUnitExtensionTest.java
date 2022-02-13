0284827e67c90bed3a6a844f96e7d38c9c351b54
Alex <azakotyansky@gmail.com>
2022-02-12 20:37:39 -0500

    Initial commit

---
+++ web-app/src/test/java/alex/trading/oms/mocks_create/JUnitExtensionTest.java
@@ -0,0 +1,29 @@
+package alex.trading.oms.mocks_create;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Map;
+
+/**
+ * Use @ExtendWith(MockitoExtension.class).
+ * In this case we don't need to have
+ *
+ * @BeforeEach
+ *     void setUp() {
+ *         MockitoAnnotations.initMocks(this);
+ *     }
+ */
+@ExtendWith(MockitoExtension.class)
+public class JUnitExtensionTest {
+
+    @Mock
+    Map<String, Object> mapMock;
+
+    @Test
+    void testMock() {
+        mapMock.put("keyvalue", "foo");
+    }
+}
